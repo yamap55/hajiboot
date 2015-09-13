e4175bc9dbb74679a8ee6a3f09df3d6954db5ae4
yamap55 <tetuya120@hotmail.com>
2015-09-14 00:12:41 +0900

    hajiboot 2.1.1完了。

--- hajiboot-di/src/main/java/com/example/App.java
+++ hajiboot-di/src/main/java/com/example/App.java
@@ -1,13 +1,26 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.Scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Import;
+
+import com.example.app.Calculator;
+
+@EnableAutoConfiguration
+@Import(AppConfig.class)
+public class App {
+	public static void main(String[] args) {
+		try (ConfigurableApplicationContext context = SpringApplication.run(App.class, args)) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter 2 number like 'a b' : ");
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+			Calculator calculator = context.getBean(Calculator.class);
+			int resule = calculator.calc(a, b);
+			System.out.println("result = " + resule);
+		}
+	}
 }
---
+++ hajiboot-di/src/main/java/com/example/app/AddCalculator.java
@@ -0,0 +1,10 @@
+package com.example.app;
+
+public class AddCalculator implements Calculator {
+
+	@Override
+	public int calc(int a, int b) {
+		return a + b;
+	}
+
+}
