6f75821a1b15ea65cc0333a15216e8b4a7e60893
yamap55 <tetuya120@hotmail.com>
2015-09-14 23:04:21 +0900

    hajiboot 2.1.2 完了。

--- hajiboot-di/src/main/java/com/example/App.java
+++ hajiboot-di/src/main/java/com/example/App.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
 
+import com.example.app.Argument;
+import com.example.app.ArgumentResolver;
 import com.example.app.Calculator;
 
 @EnableAutoConfiguration
@@ -16,10 +18,10 @@ public class App {
 		try (ConfigurableApplicationContext context = SpringApplication.run(App.class, args)) {
 			Scanner scanner = new Scanner(System.in);
 			System.out.println("Enter 2 number like 'a b' : ");
-			int a = scanner.nextInt();
-			int b = scanner.nextInt();
+			ArgumentResolver argumentResolver = context.getBean(ArgumentResolver.class);
+			Argument argument = argumentResolver.resolve(System.in);
 			Calculator calculator = context.getBean(Calculator.class);
-			int resule = calculator.calc(a, b);
+			int resule = calculator.calc(argument.getA(), argument.getB());
 			System.out.println("result = " + resule);
 		}
 	}
--- hajiboot-di/src/main/java/com/example/AppConfig.java
+++ hajiboot-di/src/main/java/com/example/AppConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.example.app.AddCalculator;
+import com.example.app.ArgumentResolver;
 import com.example.app.Calculator;
+import com.example.app.ScannerArgumentResolver;
 
 @Configuration
 public class AppConfig {
@@ -13,4 +15,8 @@ public class AppConfig {
 		return new AddCalculator();
 	}
 
+	@Bean
+	ArgumentResolver argumentResolver() {
+		return new ScannerArgumentResolver();
+	}
 }
