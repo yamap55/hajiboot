228c095fac6034d7b56cad31aa083292d881deaf
yamap55 <tetuya120@hotmail.com>
2015-09-15 22:42:34 +0900

    hajiboot 2.1.4完了。

--- hajiboot-di/src/main/java/com/example/app/ScannerArgumentResolver.java
+++ hajiboot-di/src/main/java/com/example/app/ScannerArgumentResolver.java
@@ -3,6 +3,9 @@ package com.example.app;
 import java.io.InputStream;
 import java.util.Scanner;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ScannerArgumentResolver implements ArgumentResolver {
 
 	@Override
