cbe49bcc9808bd3affd3b435548ff9290d55bcf0
yamap55 <tetuya120@hotmail.com>
2015-09-16 22:50:45 +0900

    hajiboot 2.1.5完了。

--- hajiboot-di/src/main/java/com/example/App.java
+++ hajiboot-di/src/main/java/com/example/App.java
@@ -1,20 +1,34 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.example.app.Frontend;
+import com.example.app.Argument;
+import com.example.app.ArgumentResolver;
+import com.example.app.Calculator;
 
 @EnableAutoConfiguration
 @ComponentScan
-public class App {
-	public static void main(String[] args) {
-		try (ConfigurableApplicationContext context = SpringApplication.run(App.class, args)) {
+public class App implements CommandLineRunner {
+	@Autowired
+	ArgumentResolver argumentResolver;
+	@Autowired
+	Calculator calculator;
 
-			Frontend frontend = context.getBean(Frontend.class);
-			frontend.run();
-		}
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Enter 2 number like 'a b' : ");
+
+		Argument argument = argumentResolver.resolve(System.in);
+		int result = calculator.calc(argument.getA(), argument.getB());
+		System.out.println("result = " + result);
+
 	}
 }
