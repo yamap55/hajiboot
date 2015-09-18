3f9b81f1d48950f4c24f0e526234142c7c236574
yamap55 <tetuya120@hotmail.com>
2015-09-18 23:35:14 +0900

    hajiboot 2.2.1 パラメータなし。

--- hajiboot-jdbc/src/main/java/com/example/App.java
+++ hajiboot-jdbc/src/main/java/com/example/App.java
@@ -1,13 +1,28 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+@EnableAutoConfiguration
+public class App implements CommandLineRunner {
+	@Autowired
+	NamedParameterJdbcTemplate jdbcTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		String sql = "SELECT 1";
+		SqlParameterSource parameterSource = new MapSqlParameterSource();
+		Integer result = jdbcTemplate.queryForObject(sql, parameterSource, Integer.class);
+
+		System.out.println("result = " + result);
+	}
 }
