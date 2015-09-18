99b229e60c390003bea51bd3fde1d08fd2b28561
yamap55 <tetuya120@hotmail.com>
2015-09-19 00:00:46 +0900

    匿名クラスをラムダ式に変更。

--- hajiboot-jdbc/src/main/java/com/example/App.java
+++ hajiboot-jdbc/src/main/java/com/example/App.java
@@ -1,13 +1,9 @@
 package com.example;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -27,12 +23,9 @@ public class App implements CommandLineRunner {
 	public void run(String... arg0) throws Exception {
 		String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
 		SqlParameterSource param = new MapSqlParameterSource().addValue("id", 1);
-		Customer result = jdbcTemplate.queryForObject(sql, param, new RowMapper<Customer>() {
-			@Override
-			public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
-				return new Customer(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"));
-			}
-		});
+
+		Customer result = jdbcTemplate.queryForObject(sql, param,
+				(rs, rowNum) -> new Customer(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name")));
 
 		System.out.println("result = " + result);
 	}
