e866ef22f9457e5772312a558ae27d1456466483
yamap55 <tetuya120@hotmail.com>
2015-09-19 22:33:13 +0900

    hajiboot 2.2.3 完了。

--- hajiboot-jdbc/src/main/java/com/example/App.java
+++ hajiboot-jdbc/src/main/java/com/example/App.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -11,6 +12,7 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import com.example.domain.Customer;
 
 @EnableAutoConfiguration
+@ComponentScan
 public class App implements CommandLineRunner {
 	@Autowired
 	NamedParameterJdbcTemplate jdbcTemplate;
