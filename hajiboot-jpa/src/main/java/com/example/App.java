c4505753fb0f019b9c6d2f4cd3a581cd03099053
yamap55 <tetuya120@hotmail.com>
2015-09-21 19:41:30 +0900

    hajiboot 2.3.2完了。

--- hajiboot-jpa/src/main/java/com/example/App.java
+++ hajiboot-jpa/src/main/java/com/example/App.java
@@ -1,13 +1,29 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.example.domain.Customer;
+import com.example.repository.CustomerRepository;
+
+@EnableAutoConfiguration
+@ComponentScan
+public class App implements CommandLineRunner {
+	@Autowired
+	CustomerRepository customerRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		Customer created = customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
+		System.out.println(created + "is Created!");
+
+		customerRepository.findAll().forEach(System.out::println);
+	}
 }
