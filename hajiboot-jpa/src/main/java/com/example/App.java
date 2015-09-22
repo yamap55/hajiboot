d3b15d584c892e90e68887a1016ed315e70b1504
yamap55 <tetuya120@hotmail.com>
2015-09-22 13:02:16 +0900

    ページング処理追加。

--- hajiboot-jpa/src/main/java/com/example/App.java
+++ hajiboot-jpa/src/main/java/com/example/App.java
@@ -5,6 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import com.example.domain.Customer;
 import com.example.repository.CustomerRepository;
@@ -24,6 +27,16 @@ public class App implements CommandLineRunner {
 		Customer created = customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
 		System.out.println(created + "is Created!");
 
-		customerRepository.findAllOrderByName().forEach(System.out::println);
+		// ページング
+		Pageable pageable = new PageRequest(0, 3);
+		Page<Customer> page = customerRepository.findAll(pageable);
+
+		System.out.println("1ページあたりのデータ数=" + page.getSize());
+		System.out.println("現在のページ=" + page.getNumber());
+		System.out.println("全ページ数=" + page.getTotalPages());
+		System.out.println("全データ数=" + page.getTotalElements());
+		page.getContent().forEach(System.out::println);
+
+		// customerRepository.findAllOrderByName().forEach(System.out::println);
 	}
 }
