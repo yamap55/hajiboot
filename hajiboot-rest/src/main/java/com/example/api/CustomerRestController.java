4baf1d8cc2e1008d0c79be10cd7f6e82ac0c1701
yamap55 <tetuya120@hotmail.com>
2015-09-24 23:05:09 +0900

    hajiboot 3.2.2 完了。

--- hajiboot-rest/src/main/java/com/example/api/CustomerRestController.java
+++ hajiboot-rest/src/main/java/com/example/api/CustomerRestController.java
@@ -1,15 +1,19 @@
 package com.example.api;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.example.domain.Customer;
 import com.example.service.CustomerService;
@@ -35,11 +39,11 @@ public class CustomerRestController {
 	}
 
 	// 顧客新規作成
-	@RequestMapping(method = RequestMethod.POST)
-	@ResponseStatus(HttpStatus.CREATED)
-	Customer postCustomers(@RequestBody Customer customer) {
-		return customerService.create(customer);
-	}
+	// @RequestMapping(method = RequestMethod.POST)
+	// @ResponseStatus(HttpStatus.CREATED)
+	// Customer postCustomers(@RequestBody Customer customer) {
+	// return customerService.create(customer);
+	// }
 
 	// 顧客1件更新
 	@RequestMapping(value = "{id}", method = RequestMethod.PUT)
@@ -53,5 +57,14 @@ public class CustomerRestController {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	void deleteCustomer(@PathVariable Integer id) {
 		customerService.delete(id);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	ResponseEntity<Customer> postCustomers(@RequestBody Customer customer, UriComponentsBuilder uriBuilder) {
+		Customer created = customerService.create(customer);
+		URI location = uriBuilder.path("api/customers/{id}").buildAndExpand(created.getId()).toUri();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(location);
+		return new ResponseEntity<>(created, headers, HttpStatus.CREATED);
 	}
 }
