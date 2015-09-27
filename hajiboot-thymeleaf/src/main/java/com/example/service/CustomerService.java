feab23d480cc66b52cd26a157ddcc279538de546
yamap55 <tetuya120@hotmail.com>
2015-09-27 22:17:17 +0900

    hajiboot 3.3.1 完了。

--- hajiboot-thymeleaf/src/main/java/com/example/service/CustomerService.java
+++ hajiboot-thymeleaf/src/main/java/com/example/service/CustomerService.java
@@ -1,5 +1,7 @@
 package com.example.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,6 +19,10 @@ public class CustomerService {
 
 	public Page<Customer> findAll(Pageable pageable) {
 		return customerRepository.findAllOrderByName(pageable);
+	}
+
+	public List<Customer> findAll() {
+		return customerRepository.findAllOrderByName();
 	}
 
 	public Customer findOne(Integer id) {
