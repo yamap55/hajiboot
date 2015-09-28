8d823bb66dac8560f03b890a6a1d7f836975dadd
yamap55 <tetuya120@hotmail.com>
2015-09-28 22:15:42 +0900

    hajiboot 3.3.2 css以外実装。

--- hajiboot-thymeleaf/src/main/java/com/example/web/CustomerController.java
+++ hajiboot-thymeleaf/src/main/java/com/example/web/CustomerController.java
@@ -2,9 +2,13 @@ package com.example.web;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,10 +22,25 @@ public class CustomerController {
 	@Autowired
 	CustomerService customerService;
 
+	@ModelAttribute
+	CustomerForm setUpForm() {
+		return new CustomerForm();
+	}
+
 	@RequestMapping(method = RequestMethod.GET)
 	String list(Model model) {
 		List<Customer> customers = customerService.findAll();
 		model.addAttribute("customers", customers);
 		return "customers/list";
+	}
+
+	String create(@Validated CustomerForm form, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return list(model);
+		}
+		Customer customer = new Customer();
+		BeanUtils.copyProperties(form, customer);
+		customerService.create(customer);
+		return "redirect:/customers";
 	}
 }
