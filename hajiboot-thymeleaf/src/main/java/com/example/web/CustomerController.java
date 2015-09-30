cc0b6309d9b312f68bba66ddbe61823eda06a6c0
yamap55 <tetuya120@hotmail.com>
2015-09-30 23:10:28 +0900

    hajiboot 3.3.4 完了。

--- hajiboot-thymeleaf/src/main/java/com/example/web/CustomerController.java
+++ hajiboot-thymeleaf/src/main/java/com/example/web/CustomerController.java
@@ -70,4 +70,10 @@ public class CustomerController {
 	String goToTop() {
 		return "redirect:/customers";
 	}
+
+	@RequestMapping(value = "delete", method = RequestMethod.POST)
+	String delete(@RequestParam Integer id) {
+		customerService.delete(id);
+		return "redirect:/customers";
+	}
 }
