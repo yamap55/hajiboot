ea18e51904c1dc813bb46e25938f883f00489abc
yamap55 <tetuya120@hotmail.com>
2015-10-10 22:18:25 +0900

    hajiboot 3.5.2 完了。

---
+++ hajiboot-security/src/main/java/com/example/service/LoginUserDetails.java
@@ -0,0 +1,15 @@
+package com.example.service;
+
+import org.springframework.security.core.authority.AuthorityUtils;
+
+import com.example.domain.User;
+
+public class LoginUserDetails extends org.springframework.security.core.userdetails.User {
+	private final User user;
+
+	public LoginUserDetails(User user) {
+		super(user.getUsername(), user.getEncodePassword(), AuthorityUtils.createAuthorityList("ROLE_USER"));
+		this.user = user;
+	}
+
+}
