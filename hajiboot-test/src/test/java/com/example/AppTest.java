e07373bba9e8491e329beb5ac9cbac7c3f2e4639
yamap55 <tetuya120@hotmail.com>
2015-10-21 23:12:11 +0900

    hajiboot 5.1 完了。

--- hajiboot-test/src/test/java/com/example/AppTest.java
+++ hajiboot-test/src/test/java/com/example/AppTest.java
@@ -1,38 +1,51 @@
 package com.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.RestTemplate;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = App.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
+public class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Value("${local.server.port}")
+	int port;
+
+	RestTemplate restTemplate = new RestTemplate();
+
+	@Test
+	public void testHome() {
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port, String.class);
+
+		assertThat(response.getStatusCode(), is(HttpStatus.OK));
+		assertThat(response.getBody(), is("Hello World!!!"));
+	}
+
+	@Test
+	public void testHoge() {
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port + "/hoge", String.class);
+
+		assertThat(response.getStatusCode(), is(HttpStatus.OK));
+		assertThat(response.getBody(), is("hoge"));
+	}
+
+	@Test
+	public void testHuga() {
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port + "/huga", String.class);
+
+		assertThat(response.getStatusCode(), is(HttpStatus.OK));
+		assertThat(response.getBody(), is("huga"));
+	}
 }
